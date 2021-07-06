cb174458f29c1ea7e1f48f8bd27589171cae5d9e
Marvin Kome <marvinkome@gmail.com>
2021-07-06 23:34:55 +0200

    complete landing page v1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,7 +15,7 @@ function MyApp({ Component, pageProps }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;900&display=swap"
           rel="stylesheet"
         />
       </Head>
