d5497f434e839ad951a143a0a778c2f1910aaf81
Marvin Kome <marvinkome@gmail.com>
2021-07-29 09:24:24 +0200

    add prod contracts

--- src/config/contracts.ts
+++ src/config/contracts.ts
@@ -4,6 +4,7 @@ import ReferralABI from "config/abis/Referral.json";
 import FenixABI from "config/abis/Fenix.json";
 import RedeemABI from "config/abis/Redeem.json";
 import ERC20ABI from "config/abis/ERC20.json";
+import { DEFAULT_CHAIN_ID } from "config/constants";
 
 export type ContractInfo = {
   address: string;
@@ -17,23 +18,38 @@ export const erc20: (address: string) => ContractInfo = (address: string) => ({
 
 export default {
   referral: {
-    address: "0x8295CCCA26e2e4396061515B0b72731BDf5796C1",
+    address: {
+      137: "0xFe70D2B9647e4b020C33BE7Cf3E9F67dffc4ea76",
+      80001: "0x8295CCCA26e2e4396061515B0b72731BDf5796C1",
+    }[DEFAULT_CHAIN_ID],
     abi: ReferralABI,
   },
   masterChef: {
-    address: "0xA3dc6b430A90D61e7f1C7800d43Bd67b8a06D597",
+    address: {
+      137: "0xfABc13d1D70068D4309D88D5EdC8ddc6F36080D8",
+      80001: "0xA3dc6b430A90D61e7f1C7800d43Bd67b8a06D597",
+    }[DEFAULT_CHAIN_ID],
     abi: MasterChefABI,
   },
   irisToken: {
-    address: "0x5EB25908Abf8764CB101bE704d6Bb4a8d5254f72",
+    address: {
+      137: "0xcb2f005e307dd53493d1b509Bd08965a1dC43290",
+      80001: "0x5EB25908Abf8764CB101bE704d6Bb4a8d5254f72",
+    }[DEFAULT_CHAIN_ID],
     abi: IrisTokenABI,
   },
   fenixToken: {
-    address: "0x807Be9676f72390bCaB19f914f770d9713a2d9e0",
+    address: {
+      137: "0x5eF0002bD58760CF6056d98BBA7396C78E0506e4",
+      80001: "0x807Be9676f72390bCaB19f914f770d9713a2d9e0",
+    }[DEFAULT_CHAIN_ID],
     abi: FenixABI,
   },
   redeem: {
-    address: "0xC481Cc926522A14Ed21077B8eEd85c7C0947F62e",
+    address: {
+      137: "0xb416088a366469386f0c774C1C700a3950360D6c",
+      80001: "0xC481Cc926522A14Ed21077B8eEd85c7C0947F62e",
+    }[DEFAULT_CHAIN_ID],
     abi: RedeemABI,
   },
 };
