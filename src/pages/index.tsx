f4eba926423ee6eae8c4df7f2a0af2b864b6d1f3
Marvin Kome <marvinkome@gmail.com>
2021-08-16 20:26:45 +0200

    remove pre-sale from landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -53,11 +53,6 @@ interface NavItem {
 
 const NAV_ITEMS: Array<NavItem> = [
   {
-    label: "Pre Sale",
-    href: "/app/pre-sale",
-    decorate: true,
-  },
-  {
     label: "Info",
     children: [
       {
