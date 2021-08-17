f01b2ea4d62d37175e502c62644205df1aeba4dd
Marvin Kome <marvinkome@gmail.com>
2021-08-17 17:31:00 +0200

    change env names

--- src/pages/api/tvl-calculator.ts
+++ src/pages/api/tvl-calculator.ts
@@ -17,8 +17,8 @@ import { Token } from "quickswap-sdk";
 import { fetchPairPrice, fetchPrice } from "web3-functions/prices";
 
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_KEY_ID,
+  accessKeyId: process.env.HERMES_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.HERMES_AWS_SECRET_KEY_ID,
 });
 
 async function calculateTVL() {
