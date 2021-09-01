14da1747552d3140882bf60959c3181cfbbdd736
Marvin Kome <marvinkome@gmail.com>
2021-09-01 23:02:40 +0200

    add old kavian card

--- src/config/stake.ts
+++ src/config/stake.ts
@@ -59,6 +59,25 @@ export const stakingPools: StakeInfo[] = {
     },
 
     {
+      address: "0x7a99d3c5f6aafa527c7d58d579100284ba5f2d1a",
+      active: false,
+
+      stakeToken: {
+        address: "0xdab35042e63e93cc8556c9bae482e5415b5ac4b1",
+        symbol: "IRIS",
+        decimal: 18,
+        logo: "/hermes-logo-1.png",
+      },
+
+      rewardToken: {
+        address: "0xC4Df0E37e4ad3e5C6D1dF12d3Ca7Feb9d2B67104",
+        symbol: "KAVIAN (old)",
+        decimal: 18,
+        logo: "/kavian-logo.png",
+      },
+    },
+
+    {
       address: "0xCa0eC1fAE7335469055A3e8B85a21D4CF6bf3F5d",
       active: false,
 
