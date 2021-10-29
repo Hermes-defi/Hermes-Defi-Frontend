58dde6c282e22bf9ca509536984d3e5cba0ff689
Marvin Kome <marvinkome@gmail.com>
2021-10-29 17:44:19 +0200

    Update constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 export const DEFAULT_CHAIN_ID = isProd ? 137 : 137; //80001;
 export const BurnAddress = "0x000000000000000000000000000000000000dEaD";
-export const apolloPerBlock = 4.1;
+export const apolloPerBlock = 4;
 export const secondsPerBlock = 2.1;
 export const secondsPerWeek = 604800;
 export const secondsPerYear = 31536000;
