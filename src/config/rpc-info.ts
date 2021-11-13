3bb94954faecc4ce822a889d27ec7fa7e10b35ff
devbrooklyn <devbrooklyn@protonmail.com>
2021-11-13 08:59:27 -0300

    change network rpc details

--- src/config/rpc-info.ts
+++ src/config/rpc-info.ts
@@ -1,46 +1,37 @@
 export const RPC_INFO = {
-  137: {
-    chainName: "Matic Network",
+  1666600000: {
+    chainName: "Harmony Mainnet Shard 0",
     nativeCurrency: {
-      name: "MATIC",
-      symbol: "MATIC",
+      name: "ONE",
+      symbol: "ONE",
       decimals: 18,
     },
     rpcUrls: [
-      "https://rpc-mainnet.matic.network",
-      "https://rpc-mainnet.maticvigil.com",
-      "https://rpc-mainnet.matic.quiknode.pro",
-      "https://matic-mainnet.chainstacklabs.com",
-      "https://matic-mainnet-full-rpc.bwarelabs.com",
-      "https://matic-mainnet-archive-rpc.bwarelabs.com",
+      "https://api.harmony.one",
+      "https://s1.api.harmony.one",
+      "https://s2.api.harmony.one",
+      "https://s3.api.harmony.one",
     ],
     blockExplorerUrls: [
-      "https://polygonscan.com/",
-      "https://polygon-explorer-mainnet.chainstacklabs.com/",
-      "https://explorer-mainnet.maticvigil.com/",
-      "https://explorer.matic.network/",
-      "https://backup-explorer.matic.network/",
+      "https://explorer.harmony.one/"
     ],
   },
 
-  80001: {
-    chainName: "Mumbai",
+  1666700001: {
+    chainName: "Harmony Testnet",
     nativeCurrency: {
-      name: "MATIC",
-      symbol: "MATIC",
+      name: "ONE",
+      symbol: "ONE",
       decimals: 18,
     },
     rpcUrls: [
-      "https://rpc-mumbai.maticvigil.com/",
-      "https://matic-mumbai.chainstacklabs.com",
-      "https://matic-testnet-archive-rpc.bwarelabs.com",
+      "https://api.harmony.one",
+      "https://s1.api.harmony.one",
+      "https://s2.api.harmony.one",
+      "https://s3.api.harmony.one",
     ],
     blockExplorerUrls: [
-      "https://mumbai.polygonscan.com/",
-      "https://polygon-explorer-mumbai.chainstacklabs.com/",
-      "https://explorer-mumbai.maticvigil.com/",
-      "https://mumbai-explorer.matic.today/",
-      "https://backup-mumbai-explorer.matic.today/",
+      "https://explorer.harmony.one/"
     ],
   },
 };
