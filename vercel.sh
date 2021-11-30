ca2faf25db3d69920586f125be9a639c591ecf66
poseidonOG <poseidonisanOG@gmail.com>
2021-11-30 18:41:22 +0100

    Vercel script updated

--- vercel.sh
+++ vercel.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [[ $VERCEL_GIT_COMMIT_REF == "hermes-layer-plutus" ]] ; then
-  echo ">> Skipping deploy!"
-  exit 0;
-else
+if [[ $VERCEL_GIT_COMMIT_REF == "hermes-layer-apollo" || $VERCEL_GIT_COMMIT_REF == "main" || $VERCEL_GIT_COMMIT_REF == "hermes-layer-1"]] ; then
   echo ">> Proceeding with deploy."
   exit 1; 
+else
+  echo ">> Skipping deploy!"
+  exit 0;
 fi
