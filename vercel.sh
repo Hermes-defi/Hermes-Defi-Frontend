ec2ad8cbc4824e8132fd0a756b6b94ce53cbccc0
amaikol <poseidonisanOG@gmail.com>
2021-12-20 01:21:00 +0100

    Update vercel.sh

--- vercel.sh
+++ vercel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "hermes-layer-apollo" || "$VERCEL_GIT_COMMIT_REF" == "main" || "$VERCEL_GIT_COMMIT_REF" == "hermes-layer-1" ]] ; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "hermes-layer-plutus" || "$VERCEL_GIT_COMMIT_REF" == "main" || "$VERCEL_GIT_COMMIT_REF" == "hermes-layer-1" ]] ; then
   echo ">> Proceeding with deploy."
   exit 1; 
 else
