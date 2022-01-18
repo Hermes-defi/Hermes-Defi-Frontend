cdb29eed98333f49917a1968e318b853609ce8db
a.maik <poseidonisanog@gmail.com>
2022-01-18 16:12:59 +0100

    Final vaults

--- src/config/vaults.ts
+++ src/config/vaults.ts
@@ -72,101 +72,10 @@ export type Vault = {
 
 export const vaults: Vault[] = {
   1666600000: [
-    // {
-    //   address: "0x0B4F4dD7aF72d4Ac34BCb7979856757371Be0AE5",
-    //   stratAddress: "0x61fc9316D5Cd856ECf15064E922a014D2d690494",
-    //   masterChefAddress: defaultContracts.masterChef.address,
-    //   farmPid: 1,
-    //   tokenPerBlock: "400000000000000000",
-
-    //   isActive: true,
-    //   amm: "sushiswap",
-    //   depositFees: 0,
-    //   withdrawFees: 0,
-    //   performanceFee: 0.025,
-
-    //   projectToken: {
-    //     address: "0xd32858211fcefd0be0dd3fd6d069c3e821e0aef3",
-    //     symbol: "PLTS",
-    //     decimals: 18,
-    //     logo: "/plutus-logo.png",
-    //   },
-
-    //   stakeToken: {
-    //     address: "0x39be7c95276954a6f7070f9baa38db2123691ed0",
-    //     symbol: "1WBTC/1ETH",
-    //     decimals: 18,
-    //     logo: ["/btc-logo.png", "/eth-logo.png"],
-    //   },
-
-    //   rewardToken: {
-    //     symbol: "pSushiWBTC-ETH",
-    //     decimals: 18,
-    //   },
-
-    //   pairs: [
-    //     {
-    //       address: "0x3095c7557bcb296ccc6e363de01b760ba031f2d9",
-    //       decimals: 8,
-    //       symbol: "1WBTC",
-    //     },
-    //     {
-    //       address: "0x6983d1e6def3690c4d616b13597a09e6193ea013",
-    //       decimals: 18,
-    //       symbol: "1ETH",
-    //     },
-    //   ],
-    // },
-
-    // {
-    //   address: "0xf2eB51Fbc4A8552d8d235560Ea8bA80C1D57E663",
-    //   stratAddress: "0x96ef81dA9778b9BCB0f7a7702574C9E99af93487",
-    //   masterChefAddress: defaultContracts.masterChef.address,
-    //   farmPid: 2,
-    //   tokenPerBlock: "400000000000000000",
-
-    //   isActive: true,
-    //   amm: "sushiswap",
-    //   depositFees: 0,
-    //   withdrawFees: 0,
-    //   performanceFee: 0.025,
-
-    //   projectToken: {
-    //     address: "0xd32858211fcefd0be0dd3fd6d069c3e821e0aef3",
-    //     symbol: "PLTS",
-    //     decimals: 18,
-    //     logo: "/plutus-logo.png",
-    //   },
-
-    //   stakeToken: {
-    //     address: "0x468dc50884962D6F81733aC0c23c04611aC219F9",
-    //     symbol: "1SUSHI/WONE",
-    //     decimals: 18,
-    //     logo: ["/sushi-logo.jpeg", "/harmony-one-logo.png"],
-    //   },
-
-    //   rewardToken: {
-    //     symbol: "pSUSHI-ONE",
-    //     decimals: 18,
-    //   },
-
-    //   pairs: [
-    //     {
-    //       address: "0xbec775cb42abfa4288de81f387a9b1a3c4bc552a",
-    //       decimals: 8,
-    //       symbol: "1SUSHI",
-    //     },
-    //     {
-    //       address: "0x6983d1e6def3690c4d616b13597a09e6193ea013",
-    //       decimals: 18,
-    //       symbol: "1ETH",
-    //     },
-    //   ],
-    // },
     //WBTC/WONE
     {
-      address: "0xd49fB583441cC0Fe40b43A87d8EC4C00Cd0Fce42",
-      stratAddress: "0x023A26eE3805904b2414d9f2da3e27F82cD56f52",
+      address: "0xccb749769511BEa80a3325A57AD1c9dc9BCB61a0",
+      stratAddress: "0xCc52f13667FAbc7De18A271197321c18ea6DD9e8",
       masterChefAddress: "0x67dA5f2FfaDDfF067AB9d5F025F8810634d84287",
       farmPid: 5,
       tokenPerBlock: "400000000000000000",
@@ -217,10 +126,10 @@ export const vaults: Vault[] = {
         },
       ],
     },
-
+    //USDC/ONE
     {
-      address: "0x79C973691Ec9a0338Bae492e5D67509fC6c1161a",
-      stratAddress: "0x2746271974F4B0961Eec8941F9D0230d232A8D17",
+      address: "0xdC01AC238a0f841a7750f456BFcf1ede486ce7a1",
+      stratAddress: "0xF01E18D4F9c51a2d9bE9b0f379be428644EA12A6",
       masterChefAddress: "0x67dA5f2FfaDDfF067AB9d5F025F8810634d84287",
       farmPid: 1,
       tokenPerBlock: "400000000000000000",
@@ -271,10 +180,10 @@ export const vaults: Vault[] = {
         },
       ],
     },
-
+    //BUSD/bscBUSD
     {
-      address: "0x70a24716236eE0100Be89fff5A2B1cb454C76B92",
-      stratAddress: "0x8b135161c5153F8C0C31A32c536C3829a49BE12D",
+      address: "0xa9988C46187c0f00D8496EA5069aA5141f77eC05",
+      stratAddress: "0xa9f22dF0c0607012A1c6E47bB1a176F6AB08b184",
       masterChefAddress: "0x67dA5f2FfaDDfF067AB9d5F025F8810634d84287",
       farmPid: 4,
       tokenPerBlock: "400000000000000000",
@@ -325,10 +234,10 @@ export const vaults: Vault[] = {
         },
       ],
     },
-
+    //ETH/ONE
     {
-      address: "0xeDB5Aa5708B31c337548A5864555679738190c15",
-      stratAddress: "0x1277dB6924Bfd3E9EA0dEcd0fb91123f5f8F655C",
+      address: "0xcD87C158ca469BC01dbC1Eb33a338964873a120C",
+      stratAddress: "0x76Bd119778DF430Af675d5B37F3698fB2C097775",
       masterChefAddress: "0x67dA5f2FfaDDfF067AB9d5F025F8810634d84287",
       farmPid: 3,
       tokenPerBlock: "400000000000000000",
@@ -380,10 +289,10 @@ export const vaults: Vault[] = {
         
       ],
     },
-
+    //UST/ETH
     {
-      address: "0x563b2825Db199a727d2777FeD371e8228A2537a2",
-      stratAddress: "0x5908D34eB884Dbe0e53b0135149493EA9eE19188",
+      address: "0xE26c98dE1E148Ea07a7E7af0720cc8Ea0E1260B1",
+      stratAddress: "0x96A21c896F497F7B5fd09444F12C3589b3c6E91E",
       masterChefAddress: "0x67dA5f2FfaDDfF067AB9d5F025F8810634d84287",
       farmPid: 12,
       tokenPerBlock: "400000000000000000",
@@ -435,9 +344,10 @@ export const vaults: Vault[] = {
       ],
     },
 
+    //USDT/USDC
     {
-      address: "0x83A588379FA3521Ba87Fa4f9b979cB5677dFC4EC",
-      stratAddress: "0x17aC0362fe6954A33d772Ab660E6f885889929A7",
+      address: "0x148f943E639Fb32fd2899E1fa545B9350ace3d11",
+      stratAddress: "0x4B3d4B9742A4cEf4D22FCda11DbD3dD7fb8b047B",
       masterChefAddress: "0x67dA5f2FfaDDfF067AB9d5F025F8810634d84287",
       farmPid: 6,
       tokenPerBlock: "400000000000000000",
@@ -488,12 +398,12 @@ export const vaults: Vault[] = {
         },
       ],
     },
-
+    //SUSHI/ONE
     {
-      address: "0x4cDCBcC6eb171051075f1cd73D78A8AAd9BbcD9C",
-      stratAddress: "0x0B4F8Bb803B1a579E637C2B3B87eAbeF54a9EAd4",
+      address: "0xE78e5eEB4fBC47721fE08CaC423a2eCE6264Bf26",
+      stratAddress: "0x560b6C23DA7FE548130e4D9325162936f690F8de",
       masterChefAddress: "0x67dA5f2FfaDDfF067AB9d5F025F8810634d84287",
-      farmPid: 13,
+      farmPid: 7,
       tokenPerBlock: "400000000000000000",
 
       isActive: true,
@@ -518,27 +428,28 @@ export const vaults: Vault[] = {
       ],
 
       stakeToken: {
-        address: "0xa46bba980512e328e344ce12bb969563f3429f05",
-        symbol: "1ETH/1FRAX",
+        address: "0x468dc50884962d6f81733ac0c23c04611ac219f9",
+        symbol: "1SUSHI/WONE",
         decimals: 18,
-        logo: ["/eth-logo.png", "/1frax-logo.png"],
+        logo: ["/sushi-logo.jpeg", "/harmony-one-logo.png"],
       },
 
       rewardToken: {
-        symbol: "pSushiFRAX-ETH",
+        symbol: "pSushiSUSHI-ONE",
         decimals: 18,
       },
 
       pairs: [ 
         {
-          address: "0x6983d1e6def3690c4d616b13597a09e6193ea013",
+          address: "0xbec775cb42abfa4288de81f387a9b1a3c4bc552a",
+          symbol: "1SUSHI",
           decimals: 18,
-          symbol: "1ETH",
         },
+        
         {
-          address: "0xeb6c08ccb4421b6088e581ce04fcfbed15893ac3",
+          address: "0xcf664087a5bb0237a0bad6742852ec6c8d69a27a",
           decimals: 18,
-          symbol: "1FRAX",
+          symbol: "WONE",
         },
       ],
     },
