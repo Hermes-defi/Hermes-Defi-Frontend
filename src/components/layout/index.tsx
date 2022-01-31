b53a731be8e0e6f153a07ad36e3952ec454f6562
a.maik <poseidonisanog@gmail.com>
2022-01-31 14:09:10 +0100

    Minor changes

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -31,11 +31,10 @@ export const AppLayout: React.FC = ({ children }) => {
                   Partnerships:
                 </Text>
 
-                <Stack spacing={5} direction="row" >
-                  <Image w={9} src="/big-dfyn.svg" alt="Dfyn Logo" />
-                  <Image w={10} src="/viper-logo.png" alt="ViperSwap Logo" />
-                  <Image w={10} src="/euphoria-logo.png" alt="Euphoria Logo" />
-                  <Image w={10} src="/roy-logo.png" alt="Crypto Royale Logo" />
+                <Stack spacing={5} direction="row">
+                  <Image w={10} src="/roy-logo.png" alt="CryptoRoyale Logo" />
+                  <Image w={9} src="/ape-logo.svg" alt="Harmonape Logo" />
+                  <Image w={9} src="/lumen-logo.svg" alt="Lumen Logo" />
                 </Stack>
               </Stack>
 
