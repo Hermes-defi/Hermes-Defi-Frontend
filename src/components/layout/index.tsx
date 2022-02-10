1e88f44adbefff43a3829f251b7ca29bb95f7e45
Marvin Kome <marvinkome@gmail.com>
2022-02-10 22:29:22 +0100

    add magic logo

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -32,11 +32,15 @@ export const AppLayout: React.FC = ({ children }) => {
                   Partnerships:
                 </Text>
 
-                <Stack spacing={5} direction="row">
-                  <Image w={10} src="/roy-logo.png" alt="CryptoRoyale Logo" />
-                  <Image w={9} src="/ape-logo.svg" alt="Harmonape Logo" />
-                  <Image w={9} src="/lumen-logo.svg" alt="Lumen Logo" />
-                  <Image w={9} src="/cosmos.png" alt="Cosmos Logo" />
+                <Stack spacing={5} direction="row" pb={3} alignItems="center">
+                  <Image boxSize={9} src="/roy-logo.png" alt="CryptoRoyale Logo" />
+                  <Image boxSize={9} src="/ape-logo.svg" alt="Harmonape Logo" />
+                  <Image boxSize={9} src="/lumen-logo.svg" alt="Lumen Logo" />
+                </Stack>
+
+                <Stack spacing={5} direction="row" alignItems="center">
+                  <Image boxSize={9} src="/cosmos.png" alt="Cosmos Logo" />
+                  <Image boxSize={9} src="/magic-logo.png" alt="Magic Logo" />
                 </Stack>
               </Stack>
             </Stack>
