91065b3806ff8a97412b9281413d8aac69602a4a
Marvin Kome <marvinkome@gmail.com>
2022-02-10 20:30:20 +0100

    fix mobile view for timer

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -107,17 +107,16 @@ const Page: React.FC = () => {
     <AppLayout>
       <Stack spacing={10} py={10}>
         <Stack direction={["column", "row"]} justify="center" spacing={10}>
-          <Box align="center" w="100%" bg={useColorModeValue("white", "gray.700")} rounded="2xl" boxShadow="base" px={[5, 10]} py={6}>
+          <Box align="center" w="100%" bg={useColorModeValue("white", "gray.700")} px={[5, 10]} py={6}>
             <Heading align="center" color={useColorModeValue("primary.600", "accent.200")} fontSize="2xl">
-              Layer 2 Farming ends
+              Layer 2 Farming ends in
             </Heading>
 
-            <Badge mt={"3"} colorScheme="secondary" fontSize="2xl" size="2xl" py={2} px={10} rounded="xl">
-              <Text align="center" letterSpacing="1px" fontWeight="700">
-                {timerComponents}
-              </Text>
-            </Badge>
-            <Heading mt={"3"} align="center" color={useColorModeValue("primary.600", "accent.200")} fontSize="2xl">
+            <Text align="center" letterSpacing="1px" fontWeight="700">
+              {timerComponents.length ? timerComponents : "Farming ended"}
+            </Text>
+
+            <Heading mt={"3"} align="center" color={useColorModeValue("primary.600", "accent.200")} fontSize="xl">
               Get ready for Hermes Protocol
             </Heading>
           </Box>
