1989aa5fe1360cb7b80847515aedab537187e32d
Marvin Kome <marvinkome@gmail.com>
2022-03-31 10:35:47 +0200

    update vaults to only revalidate after 1 day

--- src/pages/app/vaults.tsx
+++ src/pages/app/vaults.tsx
@@ -233,7 +233,7 @@ export async function getStaticProps() {
     // Next.js will attempt to re-generate the page:
     // - When a request comes in
     // - At most once every 10 seconds
-    revalidate: 30, // In seconds
+    revalidate: 24 * 60 * 60, // 1 day
   };
 }
 
